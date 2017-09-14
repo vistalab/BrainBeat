f986998e378c976deb25624fe3f8800e80870d03
Dora <dora@DN0a221225.SUNet>
2017-09-14 13:16:31 -0700

    added script that includes equations from Bianciardi and reproduces their figure 1

---
+++ Bianciardi_Figure1.m
@@ -0,0 +1,54 @@
+
+
+% Implements equations from Bianciardi et al., 2015
+% Reproduces figure 1
+
+% MRI acquisition settings:
+TE = 0.0116; % echo time (in sec) .020 for 350 ms, .0116 for 250 ms
+FA_deg = [10 30 45 65 90]; % flip angle in degrees to plot
+FA_all = (FA_deg*2*pi)/360; % flip angle in radians
+
+V = 0:0.01:11; % cm/s
+Vc = 3.6;
+
+% Tissue parameters [blood WM GM CSF] from Bianciardi at 7T
+T1 = [2.5 1.2 2.1 4.4]; % seconds longitudinal recovery rate
+
+% % Tissue parameters [blood WM GM CSF] from Bianciardi at 3T
+% T1 = [1.7 0.8 1.3 4.4]; % seconds longitudinal recovery rate
+
+tt = 4; % [1:blood 2:WM 3:GM 4:CSF]
+
+%%
+figure,hold on
+for FA = FA_all
+
+    TR = 0.033;
+    G = 1; % a gain factor
+    M0 = 1;
+
+    q = exp(-TR/T1(tt))*cos(FA);
+
+    Mxy = NaN(size(V));
+
+    TE = 0;
+    T2s = 1; % displays are for TE = 0, T2s can be anything as equitions have TE/T2s;
+
+    % regime 1 v==0:
+    for kk = find(V==0)   
+        MzV0 = (M0 * (1-exp(-TR/T1(tt))))/(1-q);
+        Mxy(kk) = G * sin(FA) * MzV0 * exp(-TE/T2s);
+    end
+
+    % regime 2 v<vc:
+    for kk = find(V>0 & V<Vc) 
+        MzV0 = (M0 * (1-exp(-TR/T1(tt))))/(1-q);
+        Mxy(kk) = G * sin(FA) * (MzV0 + (M0-MzV0)*(1-q.^(Vc/V(kk)))/((Vc/V(kk))*(1-q)) )* exp(-TE/T2s);
+    end
+
+    % regime 3 v>vc:
+    for kk = find(V>=Vc) 
+        Mxy(kk) = G * sin(FA) * M0 * exp(-TE/T2s);
+    end
+    plot(V,Mxy)
+end
