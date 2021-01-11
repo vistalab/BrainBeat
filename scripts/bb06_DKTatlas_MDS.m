67e424d5be6bd663d4acb5e23c8f5c9275e2ff68
DoraHermes <hermes.dora@mayo.edu>
2021-01-11 09:50:57 -0600

    update to gitignore to ignore images and DKT atlas across subjects

--- scripts/bb06_DKTatlas_MDS.m
+++ scripts/bb06_DKTatlas_MDS.m
@@ -16,10 +16,13 @@ dDir = '/Volumes/DoraBigDrive/data/BrainBeat/data/';
 all_subs = [1 2 3 4 5 6];
 all_scans = [3 3 3 1 1 1];
 
-[roiCodes,roiNames] = dkt_labels();
+dkt_table = readtable(fullfile(dDir,'dkt_areas.tsv'),'FileType','text','Delimiter','\t','TreatAsEmpty',{'N/A','n/a'});
+roiNames = dkt_table.label;
+roiCodes = dkt_table.label_nr;
 
 t_hr = linspace(-.5,1.5,128);
 avResp_hr = NaN(length(t_hr),length(roiNames),length(all_subs));
+avRespVeno_hr = NaN(length(t_hr),1,length(all_subs));
 ppgCurve_hr = NaN(length(t_hr),length(all_subs));
     
 for ss = 1:length(all_subs)
@@ -48,21 +51,40 @@ for ss = 1:length(all_subs)
     ppgResp = niftiRead(fullfile(dDir,subj,scan,[scanName '_PPGtrigResponse.nii.gz']));
     ppgT = load(fullfile(dDir,subj,scan,[scanName '_PPGtrigResponseT.mat']));
 
+    %%%% Scale the time-series matrix by the reliability
+    % Get odd/even cod (made with bbCod/Correlate2physio):
+    ppgRname = fullfile(dDir,subj,scan,[scanName '_codPPG.nii.gz']);
+    ppgR = niftiRead(ppgRname); % COD between even and odd heartbeats
+    % Set maximum of ppgTS to 1 for each voxel
+    ppgResp.data = ppgResp.data ./ repmat(max(abs(ppgResp.data),[],4),[1,1,1,size(ppgResp.data,4)]);
+    % Multiply by cod size (always >= 0)
+    ppgResp.data = ppgResp.data .* repmat(ppgR.data,[1,1,1,size(ppgResp.data,4)]);
+    
+    % load segmentation
     niSegm = niftiRead(fullfile(dDir,subj,scan,[scanName '_r_DKTatlas_aseg.nii.gz']));
     % matrix to vector
     segmVect = reshape(niSegm.data,[size(niSegm.data,1) * size(niSegm.data,2) * size(niSegm.data,3)],1);
 
+    % also get the Venogram segmentation from niSegm2
+    % niSegm2 has labels 1:5 with names {'GM','WM','Ventricles','CSF','Veno'};
+    niSegm2 = niftiRead(fullfile(dDir,subj,scan,[scanName '_combineSegm.nii.gz']));
+    segm2Vect = reshape(niSegm2.data,[size(niSegm2.data,1) * size(niSegm2.data,2) * size(niSegm2.data,3)],1);
+    
     % put the data in a matrix Voxel X Time
     respMat = reshape(ppgResp.data,[size(ppgResp.data,1) * size(ppgResp.data,2) * size(ppgResp.data,3)],size(ppgResp.data,4));
 
     avResp = zeros(size(ppgResp.data,4),length(roiNames));
     for kk = 1:length(roiNames)
-        avResp(:,kk) = mean(respMat(ismember(segmVect,roiCodes{kk}),:),1);
+        avResp(:,kk) = mean(respMat(ismember(segmVect,roiCodes(kk)),:),1);
     end
+    
+    % get venogram response
+    avRespVeno = mean(respMat(ismember(segm2Vect,5),:),1)';
 
     % resample avResp to ppg cycle
     ppg_cycle   = 1./physioGet(physio,'PPGrate');
     avResp_sel = avResp(ppgT.t>=(0-(.5*ppg_cycle)) & ppgT.t<=1.5*ppg_cycle,:);
+    avRespVeno_sel = avRespVeno(ppgT.t>=(0-(.5*ppg_cycle)) & ppgT.t<=1.5*ppg_cycle,:);
     t_sel = ppgT.t(ppgT.t>=(0-(.5*ppg_cycle)) & ppgT.t<=1.5*ppg_cycle);
     ppgCurve_sel = ppgCurve(ppgCurveT>=(0-(.5*ppg_cycle)) & ppgCurveT<=1.5*ppg_cycle);
     ppgt_sel = ppgCurveT(ppgCurveT>=(0-(.5*ppg_cycle)) & ppgCurveT<=1.5*ppg_cycle);
@@ -70,77 +92,95 @@ for ss = 1:length(all_subs)
     t_temp = linspace(min(t_sel),max(t_sel),128);
 
     avResp_hr(:,:,ss) = interp1(t_sel,avResp_sel,t_temp);
+    avRespVeno_hr(:,:,ss) = interp1(t_sel,avRespVeno_sel,t_temp);
     ppgCurve_hr(:,ss) = interp1(ppgt_sel,ppgCurve_sel,t_temp);
-
+    
 end
 
-%% get distance matric for MDS, try each subject
+%% get distance matric for MDS, try individual subjects
 % X = nanmean(avResp_hr(t_hr<0.5,:,:),3)';
 
 s_nr = 2;
-X = avResp_hr(t_hr<0.5,:,s_nr)';
+t_select = find(t_hr<0.5);
 
-missing_areas = isnan(X(:,1));
+X = avResp_hr(t_select,:,s_nr)';
+t_D = t_hr(t_select);
 
 % clear missing areas
+missing_areas = isnan(X(:,1));
 X(missing_areas,:) = [];
 roiUsed = roiNames;
 roiUsed(missing_areas) = [];
 
-t_D = t_hr(t_hr<0.5);
 
+% construct distance matrix
 D_length = sqrt(sum(X.^2,2));
 
 D_dot = (X*X');
 
 D = D_dot./(D_length*D_length');
 
-D(D>1) = 1;
+D(D>1) = 1; 
 
 D_mat = rad2deg(acos(D));
 
-[Y,eigvals] = cmdscale(round(D_mat));
+% multidimensional scaling
+% [Y,eigvals] = cmdscale(round(D_mat));
+[Y,eigvals] = mdscale(round(D_mat),2);
 
 %
 figure
-subplot(1,2,1)
-plot(eigvals)
-subplot(1,2,2)
+% subplot(1,2,1)
+% plot(eigvals)
+% subplot(1,2,2)
 plot(Y(:,1),Y(:,2),'.')
 text(Y(:,1),Y(:,2),roiUsed)
 % % xlabel('Miles')
 % % ylabel('Miles')
 
+figure('Position',[0 0 400 1000])
+imagesc(t_D,[1:length(roiUsed)],X)
+set(gca,'YTick',[1:length(roiUsed)],'YTickLabel',roiUsed,'FontSize',9,'FontName','Arial')
+
 
 %% get distance matric for MDS, try average across subjects
-X = nanmean(avResp_hr(t_hr<0.5,:,:),3)';
 
-missing_areas = isnan(X(:,1));
+t_select = find(t_hr<0.5);
+
+X = nanmean(avResp_hr(t_select,:,:),3)';
+t_D = t_hr(t_select);
+
 
 % clear missing areas
+missing_areas = isnan(X(:,1));
 X(missing_areas,:) = [];
 roiUsed = roiNames;
 roiUsed(missing_areas) = [];
 
-t_D = t_hr(t_hr<0.5);
+% add veno signal
+X(size(X,1)+1,:) = (nanmean(avRespVeno_hr(t_select,:,:),3)');
+roiUsed(length(roiUsed)+1) = {'Veno'};
 
+% construct distance matrix
 D_length = sqrt(sum(X.^2,2));
-
 D_dot = (X*X');
-
 D = D_dot./(D_length*D_length');
-
 D(D>1) = 1;
-
 D_mat = rad2deg(acos(D));
 
-% [Y,eigvals] = cmdscale(round(D_mat));
-[Y] = mdscale(round(D_mat),2); % nonclassical
+[Y,eigvals] = cmdscale(round(D_mat));
+% figure,plot(eigvals)
 
-%
+% [Y] = mdscale(round(D_mat),2); % nonclassical
+
 figure
 plot(Y(:,1),Y(:,2),'.')
-text(Y(:,1),Y(:,2),roiUsed)
+text(Y(:,1),Y(:,2),roiUsed,'FontSize',8)
+
+figure('Position',[0 0 400 1000])
+% imagesc(t_D,[1:length(roiUsed)],X,[-0.005 0.005])
+imagesc(t_D,[1:length(roiUsed)],X,[-.1 .1])
+set(gca,'YTick',[1:length(roiUsed)],'YTickLabel',roiUsed,'FontSize',9,'FontName','Arial')
 
 
 
@@ -167,4 +207,152 @@ title('PPG')
 % set(gcf,'PaperPositionMode','auto')
 % print('-painters','-r300','-dpng',[dDir '/figures/segmentation/allsubjects_FA48_tracesDKT1'])
 % print('-painters','-r300','-depsc',[dDir '/figures/segmentation/allsubjects_FA48_tracesDKT1'])
+
+
+%%
+%% get distance matric for MDS, concatinate all subjects
+
+t_select = find(t_hr<0.5);
+
+% concatinate all subjects
+X = reshape(avResp_hr(t_select,:,:),length(t_select),size(avResp_hr,2)*size(avResp_hr,3))';
+t_D = t_hr(t_select);
+
+% clear missing areas
+missing_areas = isnan(X(:,1)) | sum(X,2)==0;
+X(missing_areas,:) = [];
+roiUsed = repmat(roiNames,1,size(avResp_hr,3));
+roiUsed(missing_areas) = [];
+
+% add veno signal
+X = [X; squeeze(avRespVeno_hr(t_select,:,:))'];
+roiUsed(length(roiUsed)+1:length(roiUsed)+size(avResp_hr,3)) = repmat({'Veno'},6,1);
+
+% construct distance matrix
+D_length = sqrt(sum(X.^2,2));
+D_dot = (X*X');
+D = D_dot./(D_length*D_length');
+D(D>1) = 1;
+D_mat = rad2deg(acos(D));
+
+[Y,eigvals] = cmdscale(round(D_mat));
+% figure,plot(eigvals)
+
+% [Y] = mdscale(round(D_mat),2); % nonclassical
+%%
+
+% todo: add labels / remove subcortical?
+
+% plot
+figure,hold on
+plot(Y(:,1),Y(:,2),'.')
+text(Y(:,1),Y(:,2),roiUsed)
+
+% add Veno colors:
+these_areas = find(ismember(roiUsed,'Veno'));
+plot(Y(these_areas,1),Y(these_areas,2),'r*','MarkerSize',20)
+
+% add CSF colors:
+these_areas = find(ismember(roiUsed,{'leftlateralventricle','rightlateralventricle'}));
+plot(Y(these_areas,1),Y(these_areas,2),'g*','MarkerSize',20)
+
+% add posterior Cerebral colors:
+these_areas = find(ismember(roiUsed,'rightparahippocampal'));
+plot(Y(these_areas,1),Y(these_areas,2),'b*','MarkerSize',20)
+
+% add anterior Cerebral colors:
+these_areas = find(ismember(roiUsed,'leftcaudalanteriorcingulate'));
+plot(Y(these_areas,1),Y(these_areas,2),'m*','MarkerSize',20)
+
+% add middle Cerebral colors:
+these_areas = find(ismember(roiUsed,'rightinsula'));
+plot(Y(these_areas,1),Y(these_areas,2),'c*','MarkerSize',20)
+
+% % add CSF:
+% these_areas = find(ismember(roiUsed,'CSF'));
+% plot(Y(these_areas,1),Y(these_areas,2),'y*','MarkerSize',20)
+
+% % add ITG:
+% these_areas = find(ismember(roiUsed,'rightinferiortemporal'));
+% plot(Y(these_areas,1),Y(these_areas,2),'y*','MarkerSize',20)
+
+% set(gcf,'PaperPositionMode','auto')
+% print('-painters','-r300','-dpng',[dDir '/figures/segmentation/allSubs_MDStest'])
+% print('-painters','-r300','-depsc',[dDir '/figures/segmentation/allSubs_MDStest'])
+
+%%
+figure('Position',[0 0 400 1000])
+imagesc(t_D,[1:length(roiUsed)],X,[-0.005 0.005])
+set(gca,'YTick',[1:length(roiUsed)],'YTickLabel',roiUsed,'FontSize',9,'FontName','Arial')
+
+%% plot some responses
+
+figure('Position',[0 0 300 800])
+
+subplot(5,1,1),hold on
+plot(t_hr,zeros(size(t_hr)),'Color',[.7 .7 .7])
+plot([0 0],[-0.2 0.2],'Color',[.7 .7 .7])
+title('posterior cerebral branch')
+this_area = 45; % right parahippocampal
+plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'k','LineWidth',1)
+
+% this_area = 42; % right lingual
+% plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'Color',[.5 .5 .5])
+
+this_area = 38; % right ITG
+plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'Color',[.5 .5 .5])
+xlim([t_hr(1) t_hr(end)])
+
+subplot(5,1,2),hold on
+plot(t_hr,zeros(size(t_hr)),'Color',[.7 .7 .7])
+plot([0 0],[-0.2 0.2],'Color',[.7 .7 .7])
+
+title('anterior cerebral branch')
+this_area = 1; % caudal anterior cingulate 1 
+plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'k','LineWidth',1)
+
+% this_area = 52; % posterior cingulate 21 
+% plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'Color',[.5 .5 .5])
+
+this_area = 39; % isthmus cingulate 8 39
+plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'Color',[.5 .5 .5])
+
+subplot(5,1,3),hold on
+plot(t_hr,zeros(size(t_hr)),'Color',[.7 .7 .7])
+plot([0 0],[-0.2 0.2],'Color',[.7 .7 .7])
+
+title('middle cerebral branch')
+this_area = 62; % right insula
+plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'k','LineWidth',1)
+
+this_area = 44; % middle temporal
+plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'Color',[.5 .5 .5])
+
+% this_area = 56; % right rostral middle frontal
+% plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'g')
+
+subplot(5,1,4),hold on
+plot(t_hr,zeros(size(t_hr)),'Color',[.7 .7 .7])
+plot([0 0],[-0.2 0.2],'Color',[.7 .7 .7])
+
+title('Veins')
+plot(t_hr,squeeze(avRespVeno_hr(:,1,:)),'k','LineWidth',1)
+
+
+subplot(5,1,5),hold on
+plot(t_hr,zeros(size(t_hr)),'Color',[.7 .7 .7])
+plot([0 0],[-0.2 0.2],'Color',[.7 .7 .7])
+
+title('Lateral ventricles')
+
+% average left and lateral ventricles 74 89
+plot(t_hr,(squeeze(avResp_hr(:,74,:))+squeeze(avResp_hr(:,89,:)))/2,'k','LineWidth',1)
+
+% % right lateral ventricle
+% this_area = 89;
+% plot(t_hr,squeeze(avResp_hr(:,this_area,:)),'r')
+
+set(gcf,'PaperPositionMode','auto')
+print('-painters','-r300','-dpng',[dDir '/figures/segmentation/allSubs_tracessegm01'])
+print('-painters','-r300','-depsc',[dDir '/figures/segmentation/allSubs_tracessegm01'])
 
